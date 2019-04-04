2229dacaac66f8e4d41b0ffcd4de041ae09d1ec6
Alexandre78500 <alexandre.pikimi@gmail.com>
2019-04-04 16:01:05 +0200

    Correction ajout partie1

---
+++ Partie1/src/main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class main {
+	
+	 private static Scanner sc1;
+	 
+	 public static void main (String[] args) {
+		 
+		 System.out.println("Hello world");
+	 }
+}
